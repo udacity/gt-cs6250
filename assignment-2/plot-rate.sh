a7a5daf3cc7dc016fd4ca744a50745363432cd75
Josh Valdez <josh@jmvldz.com>
2013-12-26 15:44:43 -0600

    Added graphing script for bandwidth

---
+++ assignment-2/plot-rate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Creates a graph of the bandwidth from a bandwidth log
+
+# Usage: ./plot-rate.sh ./student_folder/bwm.txt
+# To use this script pass the path of the bandwidth log as the first argument.
+
+python util/plot_rate.py --rx \
+        --maxy 10 \
+        --xlabel 'Time (s)' \
+        --ylabel 'Rate (Mbps)' \
+        -i 's.*-eth2' \
+        -f $1 \
+        -o $dir/rate.png
