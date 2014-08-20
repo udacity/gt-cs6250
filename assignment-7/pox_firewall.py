af0555cf0bda4e46be088f4b1afac2616dd5b436
durant-udacity <durant@udacity.com>
2014-08-20 14:52:50 -0700

    Fixed indentation of comments in _handle_Conn ...

--- assignment-7/pox_firewall.py
+++ assignment-7/pox_firewall.py
@@ -40,16 +40,16 @@ class Firewall (EventMixin):
     def _handle_ConnectionUp (self, event):
         policies = self.read_policies(policyFile)
         for policy in policies.itervalues():
-        # TODO: implement the code to add a rule to block the flow
-        # between the source and destination specified in each policy
+            # TODO: implement the code to add a rule to block the flow
+            # between the source and destination specified in each policy
 
-        # Note: The policy data structure has two fields which you can
-        # access to turn the policy into a rule. policy.dl_src will
-        # give you the source mac address and policy.dl_dst will give
-        # you the destination mac address
+            # Note: The policy data structure has two fields which you can
+            # access to turn the policy into a rule. policy.dl_src will
+            # give you the source mac address and policy.dl_dst will give
+            # you the destination mac address
 
-        # Note: Set the priority for your rule to 20 so that it
-        # doesn't conflict with the learning bridge setup
+            # Note: Set the priority for your rule to 20 so that it
+            # doesn't conflict with the learning bridge setup
             pass
 
         log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
